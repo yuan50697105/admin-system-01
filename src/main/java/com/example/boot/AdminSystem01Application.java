64c4c0b8716dbfbcfbdfa74ff6cb8f8460eba5fc
yuanenzhi <710575900@qq.com>
2022-05-16 12:01:22 +0800

    Initial commit

---
+++ src/main/java/com/example/boot/AdminSystem01Application.java
@@ -0,0 +1,13 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSystem01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminSystem01Application.class, args);
+    }
+
+}
